c317f8ad22e085170c8e042c7f0115b94fac7150
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-06-30 18:17:21 +0900

    데이터 리스트 조회, 단건 조회

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 import express from "express"
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "korad",
+  password: "kor123414",
+  database: "music_list",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
 const app = express()
 const port = 3000
 
 
-const musicList = [
-    {
-      title: "ditto",
-      singer: "뉴진스",
-    },
-    {
-      title: "Butter",
-      singer: "BTS",
-    },
-  ];
+app.get("/music_list", async (req, res) => {
+    const [rows] = await pool.query("SELECT * FROM music_list ORDER BY id DESC");
   
-  app.get("/music", (req, res) => {
-    res.json(musicList);
+    res.json(rows);
+  });
+  
+  app.get("/music_list/:id", async (req, res) => {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM music_list WHERE id = ?", [
+      id,
+    ]);
+  
+    if (rows.length == 0) {
+      res.status(404).send("not found");
+      return;
+    }
+  
+    res.json(rows[0]);
   });
   
   app.listen(port, () => {
--- db.sql
+++ db.sql
@@ -1,17 +1,14 @@
-# DB 생성
 DROP DATABASE IF EXISTS music_list;
-CREATE DATABASE music_l`test`ist;
+CREATE DATABASE music_list;
 USE music_list;
 
-# 테이블 생성
 CREATE TABLE music_list (
-id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-reg_date DATETIME NOT NULL,
-title VARCHAR(200) NOT NULL,
-singer VARCHAR(50) NOT NULL
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    singer VARCHAR(50) NOT NULL
 );
 
-# 데이터 생성
 INSERT INTO music_list
 SET regDate = NOW(),
 title = 'Ditto',
@@ -26,3 +23,5 @@ INSERT INTO music_list
 SET regDate = NOW(),
 title = 'Kitsch',
 singer = '아이브';
+
+SELECT * FROM music_list;
