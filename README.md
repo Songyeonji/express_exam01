d5a1a15df8c1e21a35e024b83a4d41bc3a8baadf
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-06-30 17:41:57 +0900

    json 응답

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 필요 라이브러리 설치
 
 - express - 설치 명령어 : npm i express
-  -nodemon - 설치 명령어 : npm i -g nodemon
+- nodemon - 설치 명령어 : npm i -g nodemon
 
 # 실행 명령어
 
--- app.js
+++ app.js
@@ -2,18 +2,22 @@ import express from "express"
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 
-app.get('/about', (req, res) => {
-    res.send('About!')
+const musicList = [
+    {
+      title: "ditto",
+      singer: "뉴진스",
+    },
+    {
+      title: "Butter",
+      singer: "BTS",
+    },
+  ];
+  
+  app.get("/music", (req, res) => {
+    res.json(musicList);
   });
-
-app.get('/projects', (req, res) => {
-    res.send('projects!')
-  });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
